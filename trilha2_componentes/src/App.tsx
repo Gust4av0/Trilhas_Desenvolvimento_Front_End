88b48d4392761b11a3e970fad6db12a2d1d15c48
Gust4av0 <gustavomarcolin2005@gmail.com>
2025-04-03 19:37:17 -0300

    estilizando a trilha 2

--- trilha2_componentes/src/App.tsx
+++ trilha2_componentes/src/App.tsx
@@ -7,9 +7,28 @@ export default function Home() {
   return (
     <div>
       <h1>Exercícios React</h1>
+      <hr></hr>
+      <div className="titulo">
+      <p>Exercício 1</p>
+      </div>
       <Contador inicial={0} />
+      <hr></hr>
+      <br />
+      <div className="titulo">
+      <p>Exercício 2</p>
+      </div>
       <Saudacao nomeInicial="Gustavo" />
+      <hr></hr>
+      <br />
+      <div className="titulo">
+      <p>Exercício 3</p>
+      </div>
       <ListaTarefas tarefasIniciais={["Morgar", "teste"]} />
+      <hr></hr>
+      <br />
+      <div className="titulo">
+      <p>Exercício 4</p>
+      </div>
       <ToggleVisibilidade texto="Juca Bala!" />
     </div>
   );
